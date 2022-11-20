10d9202f0e97b526c7f2554a338de9393218ba20
Blackmarllbor0 <3100194@gmail.com>
2022-11-20 19:23:23 +0300

    add local and auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Body, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Req,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { User } from 'src/user/user.entity';
 import { AuthService } from './auth.service';
+import { IRequestUser } from './interfaces/user-request.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +19,13 @@ export class AuthController {
   @Post('register')
   public async register(@Body() dto: CreateUserDto): Promise<User> {
     return await this.authService.register(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  public async login(@Req() { user, res }: IRequestUser): Promise<User> {
+    const cookie = this.authService.getCookieWithJwtToken(user.id);
+    res.setHeader('Set-Cookie', cookie);
+    return user;
   }
 }
